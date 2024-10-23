43f29ddded8a0bbf9dc43a07caa706194a88139c
lanagaby17 <152930068+lanagaby17@users.noreply.github.com>
2024-10-22 22:04:29 -0300

    aula 22

---
+++ MissaoVisaoValores.js
@@ -0,0 +1,17 @@
+criaCartao (
+    'Missão',
+    'Saiba qual é a missão de nossa empresa!',
+    'Nossa missão é ...'
+)
+
+criaCartao (
+    'Visão',
+    'Saiba qual é a Visão de nossa empresa!',
+    'Nossa Visão é ...'
+)
+
+criaCartao (
+    'Valores',
+    'Saiba quais são os valores de nossa empresa!',
+    'Nossos valores são ...'
+)
