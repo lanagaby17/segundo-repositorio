642bf68bed4a6532332a663567d1cd0aa8bff931
lanagaby17 <152930068+lanagaby17@users.noreply.github.com>
2024-11-05 21:56:50 -0300

    aula 05/11

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 function criaCartao (item, pergunta,resposta) {
-    console.log(item, pergunta, resposta)
+    //console.log(item, pergunta, resposta)
+
+    let fleshcard = document.getElementById('fleshcard');
+    let cartao = document.createElement('article');
+
+    cartao.className = 'cartao';
+
+    cartao.innerHTML = `
+        <div class="cartao__conteudo__pergunta">
+                    <p>Saiba qual é a Visão de nossa empresa!</p>
+                </div>
+                
+                <div class="cartao__conteudo__resposta">
+                    <p>Nossa Visão é ... </p>
+                </div>
+            </div>
+    `
+    Flashcard.appendChild(cartao);
+
 }
