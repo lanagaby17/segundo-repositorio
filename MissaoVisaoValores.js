4eddc98ff5ed21d0fbc0601ee312e46f8eefe43c
lanagaby17 <152930068+lanagaby17@users.noreply.github.com>
2024-11-12 22:07:09 -0300

    aula 12/11

--- MissaoVisaoValores.js
+++ MissaoVisaoValores.js
@@ -1,17 +1,17 @@
 criaCartao (
     'Missão',
     'Saiba qual é a missão de nossa empresa!',
-    'Nossa missão é ...'
+    'Nossa missão é Proporcionar um ambiente acolhedor e inspirador, promovendo a paixão pela dança e o desenvolvimento pessoal e artistico de nossos alunos.'
 )
 
 criaCartao (
     'Visão',
     'Saiba qual é a Visão de nossa empresa!',
-    'Nossa Visão é ...'
+    'Nossa Visão é ser reconhecida como uma escola de dança de excelência, referência em qualidade de ensino e formação de bailarinos talentosos e comprometidos.'
 )
 
 criaCartao (
     'Valores',
     'Saiba quais são os valores de nossa empresa!',
-    'Nossos valores são ...'
+    'Nossos valores são EXCELÊNCIA- Buscamos a excelência em tudo que fazemos, do ensino á organização dos eventos. Respeito-Valorizamos a diversidade, o respeito mútuo e a inclusão em nossa comunidade. Criatividade- Estimulamos a criatividade e a expressão individual de nossos alunos. Compromisso- Somos comprometidos com o crescimento e bem-estar de nossos alunos, incentivando-os a alcançar eu máximo potencial.'
 )
