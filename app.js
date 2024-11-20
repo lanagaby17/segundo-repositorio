edb7831989a45a2753710fd3816916952d0bcb6d
lanagaby17 <152930068+lanagaby17@users.noreply.github.com>
2024-11-19 21:26:13 -0300

    aula 19

--- app.js
+++ app.js
@@ -1,21 +1,31 @@
-function criaCartao (item, pergunta,resposta) {
+function criaCartao(item, pergunta, resposta) {
     //console.log(item, pergunta, resposta)
-
-    let fleshcard = document.getElementById('fleshcard');
-    let cartao = document.createElement('article');
-
-    cartao.className = 'cartao';
+    let flashcard = document.getElementById('flashcard')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao'
 
     cartao.innerHTML = `
-        <div class="cartao__conteudo__pergunta">
-                    <p>Saiba qual é a Visão de nossa empresa!</p>
-                </div>
-                
-                <div class="cartao__conteudo__resposta">
-                    <p>Nossa Visão é ... </p>
-                </div>
+        <div class="cartao__conteudo">
+            <h3>${item}</h3>
+            
+            <div class="cartao__conteudo__pergunta">
+                <p>${pergunta}</p>
             </div>
+            
+            <div class="cartao__conteudo__resposta">
+                <p>${resposta}</p>
+            </div>
+        </div>
     `
-    Flashcard.appendChild(cartao);
+    let respostaEstaVisivel = false
+
+    function viraCartao() {
+        respostaEstaVisivel = !respostaEstaVisivel
+        cartao.classList.toggle('active', respostaEstaVisivel)
+    }
+
+    cartao.addEventListener('click', viraCartao)
+
+    flashcard.appendChild(cartao)
 
 }
